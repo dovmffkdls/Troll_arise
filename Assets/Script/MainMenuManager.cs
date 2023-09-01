53e50e469b61272d01ccba4489b01d83e4e7e1c7
appleline0315 <appleline0315@gmail.com>
2023-09-01 15:03:17 +0900

    메인 타이틀, Fantasy RPG 어셋 포함(일부 정리함)

---
+++ Assets/Script/MainMenuManager.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+
+    public Button buttion1InGame, button2LogIn, button3Option, button9Quit;
+    
+   
+
+
+    void Start()
+    {
+
+    }
+
+    void Awake()
+    {
+        buttion1InGame.onClick.AddListener(()=>Buttion1InGame("InGame"));
+        button2LogIn.onClick.AddListener(()=>Button2LogInGoogle("Login"));
+        button3Option.onClick.AddListener(() => Button3Option("Option"));
+        button9Quit.onClick.AddListener(() => Button9Quit("Quit"));
+    }
+
+    void Buttion1InGame(string message)
+    {
+        Debug.Log(message);
+        SceneManager.LoadScene("InGame");
+    }
+    void Button2LogInGoogle(string message)
+    {
+        Debug.Log(message);
+    }
+    void Button3Option(string message)
+    {
+        Debug.Log(message);
+    }
+    void Button9Quit(string message)
+    {
+        Debug.Log(message);
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+}
